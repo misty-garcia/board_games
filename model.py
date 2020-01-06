3be7f2f6e969c33d3432fb9a52c89e5016331245
Misty <ms.misty.garcia@gmail.com>
2020-01-06 00:35:04 -0600

    added test data to evaluate

--- model.py
+++ model.py
@@ -10,26 +10,30 @@ from sklearn.linear_model import LinearRegression
 import statsmodels.api as sm
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
-def linear_model(X_train, y_train):
-    lm=LinearRegression()
-    lm.fit(X_train,y_train)
-    lm_predictions=lm.predict(X_train)
-    return lm_predictions
 
-def evaluate(actual, model):
-    MSE = mean_squared_error(actual, model)
-    SSE = MSE*len(actual)
-    RMSE = sqrt(MSE)
-    r2 = r2_score(actual, model)
-    return MSE, SSE, RMSE, r2 
-
-def linear_model_and_eval(X_train, y_train):
+def linear_model_and_eval(X_train, X_test, y_train, y_test):
     # extract numeric values
     X_train_num = X_train.select_dtypes(np.number)
 
-    # predicted values & add to y_train
-    y_train["predict"] = linear_model(X_train_num, y_train)
+    # create object and fit
+    lm = LinearRegression()
+    lm.fit(X_train, y_train)
 
-    #evaluate 
-    mse, sse, rmse, r2  = evaluate(y_train['geek_rating'], y_train.predict)
-    return mse, sse, rmse, r2
+    train_predict = lm.predict(X_train)
+    test_predict = lm.predict(X_test)
+
+    # evaluating the model on train
+    rmse_train = sqrt(mean_squared_error(y_train, train_predict))
+    r2_train = r2_score(y_train, train_predict)
+
+    # evaluating the model on the test
+    rmse_test = sqrt(mean_squared_error(y_test, test_predict))
+    r2_test = r2_score(y_test, test_predict)
+
+    print("Train set model performance")
+    print("RMSE:", rmse_train)
+    print("  R2:", r2_train)
+    print("")
+    print("Test set model performance") 
+    print("RMSE:", rmse_test)
+    print("  R2:", r2_test)
